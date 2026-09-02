2069c0d57f9e33ec5badf6258161cdf79435a7d4
Kurt-matthew-san1co <sanicokp30@wvc.pshs.edu.ph>
2026-09-02 16:39:35 +0800

    Add files via upload

---
+++ Starship_oop.py
@@ -0,0 +1,20 @@
+class Starship:
+
+    def __init__(self, cargo_weight, final_fuel, base_weight):
+        self.cargo_weight = cargo_weight
+        self.final_fuel = final_fuel
+        self.base_weight = base_weight
+        final_fuel == base_weight + cargo_weight * 3
+        
+    def Starship(base_weight, cargo_weight):
+        base_weight = 50000
+        cargo_weight == 0
+
+    def load_cargo(cargo_weight):
+        cargo_weight += 1000
+
+    starship = Starship(50000, 0)
+    starship.load_cargo()
+    starship.load_cargo()
+    starship.load_cargo()
+
